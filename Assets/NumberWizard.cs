a179761aa2410d9ababb3738e92bb2ad3c221383
Samir Hysa <samir.hysa.work@gmail.com>
2020-05-02 19:06:21 +0200

    Mini console Game made with C# and Unity

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -9,10 +9,38 @@ public class NumberWizard : MonoBehaviour
     int max = 1000;
     int guess = 500;
     
-    
-    // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextOption();
+        } 
+        
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextOption();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Che Drago !11!1!");
+            StartGame();
+        }
+    }
+    
+    void StartGame()
+    {
+        min = 1;
+        max = 1000;
+        guess = 500;
         
         Debug.Log("welcome to number wizard");
         Debug.Log("-------------------------------------------");
@@ -31,26 +59,9 @@ public class NumberWizard : MonoBehaviour
         max = max + 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    void NextOption()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            guess = (min + max) / 2;
-            Debug.Log("Is higher or lower than..." + guess);
-        } 
-        
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            guess = (min + max) / 2;
-            Debug.Log("Is higher or lower than..." + guess);
-        }
-
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("Che Drago !11!1!");
-        }
+        guess = (min + max) / 2;
+        Debug.Log("Is higher or lower than..." + guess);
     }
 }
